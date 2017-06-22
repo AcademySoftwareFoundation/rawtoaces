b96b0b1cb16d11f4455f45499d6082c47821b152
Miaoqi Zhu <mzhu@oscars.org>
2017-06-21 18:29:01 -0700

    moves "recycle()" method out of Loop
    * moves "recycle()" method out of Loop for batch-processing

--- main.cpp
+++ main.cpp
@@ -206,7 +206,6 @@ int main(int argc, char *argv[])
                                       libraw_strerror(opts.ret) );
                     continue;
                 }
-
             }
             else
 #endif
@@ -440,8 +439,11 @@ int main(int argc, char *argv[])
                 iobuffer = 0;
             }
 #endif
-            RawProcessor.recycle();
-        }
+//            RawProcessor.recycle();
+    }
+
+    RawProcessor.recycle();
+    
     if (opts.verbosity)
     	printf ("Finished\n\n");
 

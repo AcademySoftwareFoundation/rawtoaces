93a5bca2a73406080132ebfe0544c8448195a2b6
Miaoqi Zhu <mzhu@oscars.org>
2017-10-18 16:49:21 -0700

    finishes up test cases for "DNGIdt" class
    * adds four more test cases for the last few functions in "DNGIdt" class

--- unittest/testDNGIdt.cpp
+++ unittest/testDNGIdt.cpp
@@ -116,6 +116,14 @@ BOOST_AUTO_TEST_CASE ( TestIDT_XYZtoCameraWeightedMatrix ) {
     
     DNGIdt * dng = new DNGIdt ( rawProcessor.imgdata.rawdata );
     double mirs[3] = { 158.8461538462, 350.1400560224, 153.8461538462 };
+    double matrix[9] = {
+        1.0165710542, -0.2791973987, -0.0801820653,
+        -0.4881171650,  1.3469051835,  0.1100471308,
+        -0.0607157824,  0.3270949763,  0.5439419519
+    };
+    vector < double > result = dng->XYZtoCameraWeightedMatrix ( mirs[0],
+                                                                mirs[1],
+                                                                mirs[2] );
     
     rawProcessor.recycle();
 
@@ -145,4 +153,72 @@ BOOST_AUTO_TEST_CASE ( TestIDT_FindXYZtoCameraMtx ) {
     FORI ( countSize ( matrix ) )
         BOOST_CHECK_CLOSE ( result[i], matrix[i], 1e-5 );
 };
+
+BOOST_AUTO_TEST_CASE ( TestIDT_ColorTemperatureToXYZ ) {
+    
+    DNGIdt * dng = new DNGIdt();
+    double cct = 6500.0;
+    double XYZ[3] = { 0.3135279229,  0.3235340821,  0.3629379950 };
+    vector < double > result = dng->colorTemperatureToXYZ ( cct );
+    
+    FORI ( countSize ( XYZ ) )
+        BOOST_CHECK_CLOSE ( result[i], XYZ[i], 1e-5 );
+};
+
+BOOST_AUTO_TEST_CASE ( TestIDT_MatrixRGBtoXYZ ) {
+    
+    DNGIdt * dng = new DNGIdt();
+    double XYZ[9] = {  0.952552395938, 0.000000000000, 0.000093678632, 0.343966449765, 0.728166096613, -0.072132546379, 0.000000000000, 0.000000000000, 1.008825184352 };
+    vector < double > result = dng->matrixRGBtoXYZ ( chromaticitiesACES );
+    
+    FORI ( countSize ( XYZ ) )
+        BOOST_CHECK_CLOSE ( result[i], XYZ[i], 1e-5 );
+};
+
+BOOST_AUTO_TEST_CASE ( TestIDT_GetDNGCATMatrix ) {
+    
+    LibRaw rawProcessor;
+    boost::filesystem::path pathToRaw = boost::filesystem::absolute \
+    ("../../unittest/materials/blackmagic_cinema_camera_cinemadng.dng");
+    int ret = rawProcessor.open_file ( ( pathToRaw.string() ).c_str() );
+    ret = rawProcessor.unpack();
+    
+    DNGIdt * dng = new DNGIdt ( rawProcessor.imgdata.rawdata );
+    double matrix [3][3] = {
+        {  0.9907763427, -0.0022862289,  0.0209908807 },
+        { -0.0017882434,  0.9941341374,  0.0083008330 },
+        {  0.0003777587,  0.0015609315,  1.1063201101 }
+    };
+
+    vector < vector < double > > result = dng->getDNGCATMatrix ( );
+    
+    rawProcessor.recycle();
+    
+    FORIJ ( 3, 3 )
+        BOOST_CHECK_CLOSE ( result[i][j], matrix[i][j], 1e-5 );
+};
+
+BOOST_AUTO_TEST_CASE ( TestIDT_GetDNGIDTMatrix ) {
+    
+    LibRaw rawProcessor;
+    boost::filesystem::path pathToRaw = boost::filesystem::absolute \
+    ("../../unittest/materials/blackmagic_cinema_camera_cinemadng.dng");
+    int ret = rawProcessor.open_file ( ( pathToRaw.string() ).c_str() );
+    ret = rawProcessor.unpack();
+    
+    DNGIdt * dng = new DNGIdt ( rawProcessor.imgdata.rawdata );
+    double matrix [3][3] = {
+        {  1.0536466144,  0.0039044182,  0.0049084502 },
+        { -0.4899562165,  1.3614787986,  0.1020844728 },
+        { -0.0024498461,  0.0060497128,  1.0139159537 }
+    };
+    
+    vector < vector < double > > result = dng->getDNGIDTMatrix ( );
+    
+    rawProcessor.recycle();
+    
+    FORIJ ( 3, 3 )
+        BOOST_CHECK_CLOSE ( result[i][j], matrix[i][j], 1e-5 );
+};
+
 

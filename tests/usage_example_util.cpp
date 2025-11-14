01aa696ac491c5dcaf4d8e653e88ea87df581151
Aleksandr Motsjonov <soswow@gmail.com>
2025-11-14 19:50:16 +1100

    debugging
    Signed-off-by: Aleksandr Motsjonov <soswow@gmail.com>

--- tests/usage_example_util.cpp
+++ tests/usage_example_util.cpp
@@ -10,9 +10,23 @@
 // This file contains some usage examples of the util library.
 // It has only very little unit test functionality to keep the code clean.
 
+namespace
+{
+std::string create_temp_output_dir()
+{
+    static const std::string dir_name = "rawtoaces_usage_example_output";
+    std::filesystem::path    temp_output_dir =
+        std::filesystem::temp_directory_path() / dir_name;
+    std::cout << "Output dir: " << temp_output_dir << std::endl;
+    std::filesystem::create_directories( temp_output_dir );
+    return temp_output_dir.string();
+}
+} // namespace
+
 /// Test the image converter using command line parameters for intialisation.
 void test_ImageConverter_arguments()
 {
+    std::cout << "Running test_ImageConverter_arguments" << std::endl;
     // This test fails on CI runners having an old version of OIIO.
     if ( OIIO::openimageio_version() < 30000 )
         return;
@@ -23,16 +37,12 @@ void test_ImageConverter_arguments()
         std::filesystem::absolute( image_path ).string();
 
     // Input parameters.
-    std::filesystem::path temp_output_dir =
-        std::filesystem::temp_directory_path() /
-        "rawtoaces_usage_example_output";
-    std::cout << "temp_output_dir: " << temp_output_dir << std::endl;
-    std::filesystem::create_directories( temp_output_dir );
-    std::string temp_output_dir_str = temp_output_dir.string();
-    const char *argv[]              = { "DUMMY PROGRAM PATH", "--wb-method",
-                                        "metadata",           "--mat-method",
-                                        "metadata",           "--overwrite",
-                                        "--output-dir",       temp_output_dir_str.c_str() };
+    std::string temp_output_dir = create_temp_output_dir();
+    const char *argv[]          = { "DUMMY PROGRAM PATH", "--wb-method",
+                                    "metadata",           "--mat-method",
+                                    "metadata",           "--overwrite",
+                                    "--output-dir",       temp_output_dir.c_str(),
+                                    "--verbose" };
 
     const size_t argc = sizeof( argv ) / sizeof( argv[0] );
 
@@ -53,6 +63,7 @@ void test_ImageConverter_arguments()
 /// Test the image converter, initialising the settings struct directly.
 void test_ImageConverter_settings()
 {
+    std::cout << "Running test_ImageConverter_settings" << std::endl;
     // This test fails on CI runners having an old version of OIIO.
     if ( OIIO::openimageio_version() < 30000 )
         return;
@@ -64,11 +75,13 @@ void test_ImageConverter_settings()
 
     // Configure the converter.
     rta::util::ImageConverter converter;
+    converter.settings.output_dir = create_temp_output_dir();
     converter.settings.WB_method =
         rta::util::ImageConverter::Settings::WBMethod::Metadata;
     converter.settings.matrix_method =
         rta::util::ImageConverter::Settings::MatrixMethod::Metadata;
     converter.settings.overwrite = true;
+    converter.settings.verbosity = 1;
 
     // Process an image.
     bool result = converter.process_image( absolute_image_path );

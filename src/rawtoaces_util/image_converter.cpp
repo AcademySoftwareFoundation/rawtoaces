da7fe79a6d69e2af2dbf2137be72d4a63b0fc35d
Aleksandr Motsjonov <soswow@gmail.com>
2025-11-17 22:08:52 +1100

    PR review comment address
    Signed-off-by: Aleksandr Motsjonov <soswow@gmail.com>

--- src/rawtoaces_util/image_converter.cpp
+++ src/rawtoaces_util/image_converter.cpp
@@ -338,13 +338,9 @@ bool prepare_transform_spectral(
 
         success = solver.find_illuminant( tmp_wb_multipliers );
 
-        if ( !success )
-        {
-            // TODO: Potentially remove, since this code path is not reachable due to camera lookup success in the previous step.
-            std::cerr << "ERROR: Failed to find a suitable illuminant."
-                      << std::endl;
-            return false;
-        }
+        // Expected to be true due to camera lookup success in the previous step,
+        // since lack of camera is the only way for find_illuminant to return false;
+        assert( success );
 
         if ( settings.verbosity > 0 )
         {

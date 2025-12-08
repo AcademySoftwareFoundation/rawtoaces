6523d402fae1eb0771b7fee3f7d5165db9b0152e
Anton Dukhovnikov <antond@wetafx.co.nz>
2025-12-09 09:08:50 +1300

    [python] more ImageConverter bindings
    Signed-off-by: Anton Dukhovnikov <antond@wetafx.co.nz>

--- src/bindings/py_util.cpp
+++ src/bindings/py_util.cpp
@@ -3,37 +3,193 @@
 
 #include "py_util.h"
 #include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
+#include <nanobind/ndarray.h>
 #include <rawtoaces/image_converter.h>
 
 using namespace rta::util;
 
 void util_bindings( nanobind::module_ &m )
 {
+    m.def( "collect_image_files", &collect_image_files );
+
     nanobind::class_<ImageConverter> image_converter( m, "ImageConverter" );
 
     image_converter.def( nanobind::init<>() );
 
-    // TODO: add the rest of the ImageConverter methods and properties
-    image_converter
-        .def_rw(
-            "settings",
-            &ImageConverter::settings ) // The Settings class is described below
-        .def( "process_image", &ImageConverter::process_image );
+    image_converter.def_rw( "settings", &ImageConverter::settings );
+    image_converter.def( "process_image", &ImageConverter::process_image );
+    image_converter.def(
+        "get_WB_multipliers", &ImageConverter::get_WB_multipliers );
+    image_converter.def( "get_IDT_matrix", &ImageConverter::get_IDT_matrix );
+    image_converter.def( "get_CAT_matrix", &ImageConverter::get_CAT_matrix );
+    image_converter.def(
+        "configure",
+        []( ImageConverter &converter, const std::string &input_filename ) {
+            OIIO::ParamValueList options;
+            return converter.configure( input_filename, options );
+        } );
+    image_converter.def(
+        "get_supported_illuminants",
+        &ImageConverter::get_supported_illuminants );
+    image_converter.def(
+        "get_supported_cameras", &ImageConverter::get_supported_cameras );
 
     nanobind::class_<ImageConverter::Settings> settings(
         image_converter, "Settings" );
 
-    // TODO: add the rest of the Settings' properties
-    settings.def( nanobind::init<>() )
-        .def_rw(
-            "WB_Method",
-            &ImageConverter::Settings::
-                WB_method ) // The WBMethod enum is described below
-        .def_rw( "illuminant", &ImageConverter::Settings::illuminant );
+    settings.def( nanobind::init<>() );
+    settings.def_rw( "WB_method", &ImageConverter::Settings::WB_method );
+    settings.def_rw(
+        "matrix_method", &ImageConverter::Settings::matrix_method );
+    settings.def_rw( "crop_mode", &ImageConverter::Settings::crop_mode );
+    settings.def_rw( "illuminant", &ImageConverter::Settings::illuminant );
+    settings.def_rw( "headroom", &ImageConverter::Settings::headroom );
+    settings.def_rw(
+        "custom_camera_make", &ImageConverter::Settings::custom_camera_make );
+    settings.def_rw(
+        "custom_camera_model", &ImageConverter::Settings::custom_camera_model );
+    settings.def_rw( "auto_bright", &ImageConverter::Settings::auto_bright );
+    settings.def_rw(
+        "adjust_maximum_threshold",
+        &ImageConverter::Settings::adjust_maximum_threshold );
+    settings.def_rw( "black_level", &ImageConverter::Settings::black_level );
+    settings.def_rw(
+        "saturation_level", &ImageConverter::Settings::saturation_level );
+    settings.def_rw( "half_size", &ImageConverter::Settings::half_size );
+    settings.def_rw(
+        "highlight_mode", &ImageConverter::Settings::highlight_mode );
+    settings.def_rw( "flip", &ImageConverter::Settings::flip );
+    settings.def_rw(
+        "denoise_threshold", &ImageConverter::Settings::denoise_threshold );
+    settings.def_rw( "scale", &ImageConverter::Settings::scale );
+    settings.def_rw(
+        "demosaic_algorithm", &ImageConverter::Settings::demosaic_algorithm );
+    settings.def_rw(
+        "database_directories",
+        &ImageConverter::Settings::database_directories );
+    settings.def_rw( "overwrite", &ImageConverter::Settings::overwrite );
+    settings.def_rw( "create_dirs", &ImageConverter::Settings::create_dirs );
+    settings.def_rw( "output_dir", &ImageConverter::Settings::output_dir );
+    settings.def_rw( "use_timing", &ImageConverter::Settings::use_timing );
+    settings.def_rw( "verbosity", &ImageConverter::Settings::verbosity );
 
-    // TODO: add the rest of WBMethod values, add other enums following this example
+    settings.def_prop_rw(
+        "WB_box",
+        []( const ImageConverter::Settings &settings ) {
+            std::vector<int> result( 4 );
+            for ( size_t i = 0; i < 4; i++ )
+                result[i] = settings.WB_box[i];
+            return result;
+        },
+        []( ImageConverter::Settings &settings, const std::vector<int> &box ) {
+            if ( box.size() == 4 )
+            {
+                settings.WB_box[0] = box[0];
+                settings.WB_box[1] = box[1];
+                settings.WB_box[2] = box[2];
+                settings.WB_box[3] = box[3];
+            }
+        } );
+
+    settings.def_prop_rw(
+        "custom_WB",
+        []( const ImageConverter::Settings &settings ) {
+            std::vector<float> result( 4 );
+            for ( size_t i = 0; i < 4; i++ )
+                result[i] = settings.custom_WB[i];
+            return result;
+        },
+        []( ImageConverter::Settings &settings,
+            const std::vector<float> &custom_WB ) {
+            if ( custom_WB.size() == 4 )
+            {
+                settings.custom_WB[0] = custom_WB[0];
+                settings.custom_WB[1] = custom_WB[1];
+                settings.custom_WB[2] = custom_WB[2];
+                settings.custom_WB[3] = custom_WB[3];
+            }
+        } );
+
+    settings.def_prop_rw(
+        "custom_matrix",
+        []( const ImageConverter::Settings &settings ) {
+            std::vector<std::vector<float>> result(
+                3, std::vector<float>( 3 ) );
+            for ( size_t i = 0; i < 3; i++ )
+                for ( size_t j = 0; j < 3; j++ )
+                    result[i][j] = settings.custom_matrix[i][j];
+            return result;
+        },
+        []( ImageConverter::Settings              &settings,
+            const std::vector<std::vector<float>> &matrix ) {
+            if ( matrix.size() == 3 )
+            {
+                for ( size_t i = 0; i < 3; i++ )
+                {
+                    if ( matrix[i].size() == 3 )
+                    {
+                        for ( size_t j = 0; j < 3; j++ )
+                            settings.custom_matrix[i][j] = matrix[i][j];
+                    }
+                }
+            }
+        } );
+
+    settings.def_prop_rw(
+        "crop_box",
+        []( const ImageConverter::Settings &settings ) {
+            std::vector<int> result( 4 );
+            for ( size_t i = 0; i < 4; i++ )
+                result[i] = settings.crop_box[i];
+            return result;
+        },
+        []( ImageConverter::Settings &settings, const std::vector<int> &box ) {
+            if ( box.size() == 4 )
+            {
+                settings.crop_box[0] = box[0];
+                settings.crop_box[1] = box[1];
+                settings.crop_box[2] = box[2];
+                settings.crop_box[3] = box[3];
+            }
+        } );
+
+    settings.def_prop_rw(
+        "chromatic_aberration",
+        []( const ImageConverter::Settings &settings ) {
+            std::vector<float> result( 2 );
+            for ( size_t i = 0; i < 2; i++ )
+                result[i] = settings.chromatic_aberration[i];
+            return result;
+        },
+        []( ImageConverter::Settings &settings,
+            const std::vector<float> &value ) {
+            if ( value.size() == 2 )
+            {
+                settings.chromatic_aberration[0] = value[0];
+                settings.chromatic_aberration[1] = value[1];
+            }
+        } );
+
     nanobind::enum_<ImageConverter::Settings::WBMethod>( settings, "WBMethod" )
         .value( "Metadata", ImageConverter::Settings::WBMethod::Metadata )
         .value( "Illuminant", ImageConverter::Settings::WBMethod::Illuminant )
+        .value( "Box", ImageConverter::Settings::WBMethod::Box )
+        .value( "Custom", ImageConverter::Settings::WBMethod::Custom )
+        .export_values();
+
+    nanobind::enum_<ImageConverter::Settings::MatrixMethod>(
+        settings, "MatrixMethod" )
+        .value( "Auto", ImageConverter::Settings::MatrixMethod::Auto )
+        .value( "Spectral", ImageConverter::Settings::MatrixMethod::Spectral )
+        .value( "Metadata", ImageConverter::Settings::MatrixMethod::Metadata )
+        .value( "Adobe", ImageConverter::Settings::MatrixMethod::Adobe )
+        .value( "Custom", ImageConverter::Settings::MatrixMethod::Custom )
+        .export_values();
+
+    nanobind::enum_<ImageConverter::Settings::CropMode>( settings, "CropMode" )
+        .value( "Off", ImageConverter::Settings::CropMode::Off )
+        .value( "Soft", ImageConverter::Settings::CropMode::Soft )
+        .value( "Hard", ImageConverter::Settings::CropMode::Hard )
         .export_values();
 }
--- tests/python/test_image_converter.py
+++ tests/python/test_image_converter.py
@@ -7,6 +7,7 @@ Unit tests for rawtoaces Python bindings - ImageConverter class
 """
 
 import pytest
+import array
 
 # The PYTHONPATH should be set by CMake to find the compiled module
 # No need to manually add paths here as CMake handles this via environment variables
@@ -20,6 +21,26 @@ except ImportError as e:
 class TestImageConverter:
     """Test cases for the ImageConverter class"""
 
+    def test_converter_has_helper_functions(self):
+        """Test that ImageConverter has helper functions"""
+        assert hasattr(rawtoaces, "collect_image_files")
+        assert callable(rawtoaces.collect_image_files)
+
+    def test_collect_image_files(self):
+        """Test that collect_image_files returns some images"""
+        import os
+        dir_path = os.path.join('.', 'tests', 'materials')
+        dir_path = os.path.abspath(dir_path)
+        # print(path)
+        batches = rawtoaces.collect_image_files([dir_path])
+        assert len(batches) == 2
+        assert len(batches[0]) == 0
+        files = batches[1]
+        files.sort()
+        assert len(files) == 2
+        assert files[0] == os.path.join(dir_path, 'BatteryPark.NEF')
+        assert files[1] == os.path.join(dir_path, 'blackmagic_cinema_camera_cinemadng.dng')
+        
     def test_converter_creation(self):
         """Test that ImageConverter can be instantiated"""
         converter = rawtoaces.ImageConverter()
@@ -32,11 +53,31 @@ class TestImageConverter:
         assert hasattr(converter, "settings")
         assert converter.settings is not None
 
-    def test_converter_has_process_image_method(self):
-        """Test that ImageConverter has process_image method"""
+    def test_converter_has_methods(self):
+        """Test that ImageConverter has all methods"""
         converter = rawtoaces.ImageConverter()
+        
         assert hasattr(converter, "process_image")
         assert callable(converter.process_image)
+        
+        assert hasattr(converter, "configure")
+        assert callable(converter.configure)
+                
+        assert hasattr(converter, "get_WB_multipliers")
+        assert callable(converter.get_WB_multipliers)
+        
+        assert hasattr(converter, "get_IDT_matrix")
+        assert callable(converter.get_IDT_matrix)
+                
+        assert hasattr(converter, "get_CAT_matrix")
+        assert callable(converter.get_CAT_matrix)
+                        
+        assert hasattr(converter, "get_supported_illuminants")
+        assert callable(converter.get_supported_illuminants)
+
+        assert hasattr(converter, "get_supported_cameras")
+        assert callable(converter.get_supported_cameras)
+        
 
     def test_process_image_with_invalid_path(self):
         """Test process_image with non-existent file returns False"""
@@ -62,6 +103,120 @@ class TestImageConverter:
             # If an exception is thrown, that's also acceptable behavior for invalid input
             pass
 
+    def test_configure_metadata(self):
+        """Test configure() calculates in the WB multipliers and colour transform matrices"""
+
+        import os
+        path = os.path.join('.', 'tests', 'materials', 'blackmagic_cinema_camera_cinemadng.dng')
+        converter = rawtoaces.ImageConverter()
+        converter.settings.overwrite = True
+        assert converter.configure(path)
+        wb = converter.get_WB_multipliers()
+        assert len(wb) == 4
+        assert abs(wb[0] - 1.58982515335083) < 0.0001
+        assert abs(wb[1] - 1) < 0.0001
+        assert abs(wb[2] - 1.2651821374893188) < 0.0001
+        assert abs(wb[3] - 0) < 0.0001
+
+        print("get_IDT_matrix", converter.get_IDT_matrix())
+        print("get_CAT_matrix", converter.get_CAT_matrix())
+
+        idt = converter.get_IDT_matrix()
+        cat = converter.get_CAT_matrix()
+
+        # Older versions of OIIO fail to fetch the DNG metadata, so rawtoaces
+        # picks the non-DNG path. Ideally we should restrict the supported
+        # versions of OIIO, but since that issue is unrelated to the python
+        # bindings, we'll just check that either of the two paths was
+        # successful.
+        if len(idt) == 3 and len(cat) == 0:
+            assert len(idt[0]) == 3
+            assert abs(idt[0][0] - 1.0536466144250152) < 0.0001
+            assert abs(idt[0][1] - 0.00390441818863832) < 0.0001
+            assert abs(idt[0][2] - 0.004908450238340354) < 0.0001
+            assert len(idt[1]) == 3
+            assert abs(idt[1][0] - -0.48995621645381615) < 0.0001
+            assert abs(idt[1][1] - 1.3614787985962031) < 0.0001
+            assert abs(idt[1][2] - 0.10208447284831194) < 0.0001
+            assert len(idt[2]) == 3
+            assert abs(idt[2][0] - -0.0024498461419844484) < 0.0001
+            assert abs(idt[2][1] - 0.006049712791275535) < 0.0001
+            assert abs(idt[2][2] - 1.013915953697747) < 0.0001
+        elif len(idt) == 0 and len(cat) == 3:
+            assert len(cat[0]) == 3
+            assert abs(cat[0][0] - 1.0097583639200136) < 0.0001
+            assert abs(cat[0][1] - 0.0050178093846550455) < 0.0001
+            assert abs(cat[0][2] - -0.01505838909238814) < 0.0001
+            assert len(cat[1]) == 3
+            assert abs(cat[1][0] - 0.0036602813378778347) < 0.0001
+            assert abs(cat[1][1] - 1.0030138169214682) < 0.0001
+            assert abs(cat[1][2] - -0.005980232945639982) < 0.0001
+            assert len(cat[2]) == 3
+            assert abs(cat[2][0] - -0.00029980928869024906) < 0.0001
+            assert abs(cat[2][1] - -0.0010516909063249997) < 0.0001
+            assert abs(cat[2][2] - 0.9282027962747658) < 0.0001
+        else:
+            pytest.fail("Either IDT or CAT matrix must not be empty")
+
+    def test_process_DNG(self):
+        """Test process_image() returns True for a valid DNG file"""
+        import os
+        path = os.path.join('.', 'tests', 'materials', 'blackmagic_cinema_camera_cinemadng.dng')
+        converter = rawtoaces.ImageConverter()
+        converter.settings.overwrite = True
+        assert converter.process_image(path)
+
+    def test_converter_get_WB_multipliers(self):
+        """Test uninitialised ImageConverter returns empty WB multipliers"""
+        import os
+        converter = rawtoaces.ImageConverter()
+        try:
+            wb = converter.get_WB_multipliers()
+            assert len(wb) == 0
+        except Exception:
+            pytest.fail(Exception)
+
+    def test_converter_get_IDT_matrix(self):
+        """Test uninitialised ImageConverter returns empty IDT matrix"""
+        import os
+        converter = rawtoaces.ImageConverter()
+        try:
+            idt = converter.get_IDT_matrix()
+            assert len(idt) == 0
+        except Exception:
+            pytest.fail(Exception)
+
+    def test_converter_get_CAT_matrix(self):
+        """Test uninitialised ImageConverter returns empty CAT matrix"""
+        import os
+        converter = rawtoaces.ImageConverter()
+        try:
+            cat = converter.get_CAT_matrix()
+            assert len(cat) == 0
+        except Exception:
+            pytest.fail(Exception)
+
+    def test_converter_get_supported_illuminants(self):
+        """Test uninitialised ImageConverter returns built-in illuminants"""
+        import os
+        converter = rawtoaces.ImageConverter()
+        try:
+            illuminants = converter.get_supported_illuminants()
+            assert len(illuminants) == 2
+            assert illuminants[0] == 'Day-light (e.g., D60, D6025)'
+            assert illuminants[1] == 'Blackbody (e.g., 3200K)'
+        except Exception:
+            pytest.fail(Exception)
+        
+    def test_converter_get_supported_cameras(self):
+        """Test uninitialised ImageConverter returns empty camera list"""
+        import os
+        converter = rawtoaces.ImageConverter()
+        try:
+            cameras = converter.get_supported_cameras()
+            assert len(cameras) == 0
+        except Exception:
+            pytest.fail(Exception)
 
 class TestSettings:
     """Test cases for the ImageConverter.Settings class"""
@@ -78,7 +233,133 @@ class TestSettings:
         settings = rawtoaces.ImageConverter.Settings()
         assert settings is not None
         assert isinstance(settings, rawtoaces.ImageConverter.Settings)
-
+    
+        
+    def test_settings_attributes(self):
+        """Test that Settings has all attributes"""
+        converter = rawtoaces.ImageConverter()
+        
+        converter.settings.WB_method = rawtoaces.ImageConverter.Settings.WBMethod.Metadata
+        assert converter.settings.WB_method == rawtoaces.ImageConverter.Settings.WBMethod.Metadata
+        
+        converter.settings.WB_method = rawtoaces.ImageConverter.Settings.WBMethod.Illuminant
+        assert converter.settings.WB_method == rawtoaces.ImageConverter.Settings.WBMethod.Illuminant
+        
+        converter.settings.WB_method = rawtoaces.ImageConverter.Settings.WBMethod.Box
+        assert converter.settings.WB_method == rawtoaces.ImageConverter.Settings.WBMethod.Box
+        
+        converter.settings.WB_method = rawtoaces.ImageConverter.Settings.WBMethod.Custom
+        assert converter.settings.WB_method == rawtoaces.ImageConverter.Settings.WBMethod.Custom
+        
+        converter.settings.matrix_method = rawtoaces.ImageConverter.Settings.MatrixMethod.Auto
+        assert converter.settings.matrix_method == rawtoaces.ImageConverter.Settings.MatrixMethod.Auto
+        
+        converter.settings.matrix_method = rawtoaces.ImageConverter.Settings.MatrixMethod.Spectral
+        assert converter.settings.matrix_method == rawtoaces.ImageConverter.Settings.MatrixMethod.Spectral
+        
+        converter.settings.matrix_method = rawtoaces.ImageConverter.Settings.MatrixMethod.Metadata
+        assert converter.settings.matrix_method == rawtoaces.ImageConverter.Settings.MatrixMethod.Metadata
+        
+        converter.settings.matrix_method = rawtoaces.ImageConverter.Settings.MatrixMethod.Adobe
+        assert converter.settings.matrix_method == rawtoaces.ImageConverter.Settings.MatrixMethod.Adobe
+        
+        converter.settings.matrix_method = rawtoaces.ImageConverter.Settings.MatrixMethod.Custom
+        assert converter.settings.matrix_method == rawtoaces.ImageConverter.Settings.MatrixMethod.Custom
+        
+        converter.settings.crop_mode = rawtoaces.ImageConverter.Settings.CropMode.Off
+        assert converter.settings.crop_mode == rawtoaces.ImageConverter.Settings.CropMode.Off
+        
+        converter.settings.crop_mode = rawtoaces.ImageConverter.Settings.CropMode.Soft
+        assert converter.settings.crop_mode == rawtoaces.ImageConverter.Settings.CropMode.Soft
+        
+        converter.settings.crop_mode = rawtoaces.ImageConverter.Settings.CropMode.Hard
+        assert converter.settings.crop_mode == rawtoaces.ImageConverter.Settings.CropMode.Hard
+        
+        converter.settings.illuminant = "illuminant"
+        assert converter.settings.illuminant == "illuminant"
+                
+        converter.settings.headroom = 1.5
+        assert converter.settings.headroom == 1.5
+                
+        converter.settings.custom_camera_make = "custom_camera_make"
+        assert converter.settings.custom_camera_make == "custom_camera_make"
+        
+        converter.settings.custom_camera_model = "custom_camera_model"
+        assert converter.settings.custom_camera_model == "custom_camera_model"
+                
+        converter.settings.auto_bright = True
+        assert converter.settings.auto_bright == True
+                        
+        converter.settings.adjust_maximum_threshold = True
+        assert converter.settings.adjust_maximum_threshold == True
+                        
+        converter.settings.black_level = 123
+        assert converter.settings.black_level == 123
+                        
+        converter.settings.saturation_level = 1234
+        assert converter.settings.saturation_level == 1234
+                        
+        converter.settings.half_size = True
+        assert converter.settings.half_size == True
+                        
+        converter.settings.highlight_mode = 2
+        assert converter.settings.highlight_mode == 2
+                        
+        converter.settings.flip = 3
+        assert converter.settings.flip == 3
+                        
+        converter.settings.denoise_threshold = 1.23
+        assert abs(converter.settings.denoise_threshold - 1.23) < 0.001
+                                
+        converter.settings.scale = 2.34
+        assert abs(converter.settings.scale - 2.34) < 0.001
+                                        
+        converter.settings.demosaic_algorithm = "AHD"
+        assert converter.settings.demosaic_algorithm == "AHD"
+                                        
+        converter.settings.database_directories = ["dir1", "dir2"]
+        assert converter.settings.database_directories == ["dir1", "dir2"]
+                                        
+        converter.settings.overwrite = True
+        assert converter.settings.overwrite == True
+                                        
+        converter.settings.create_dirs = True
+        assert converter.settings.create_dirs == True
+                                        
+        converter.settings.output_dir = "output_dir"
+        assert converter.settings.output_dir == "output_dir"
+                                        
+        converter.settings.use_timing = True
+        assert converter.settings.use_timing == True
+                                                
+        converter.settings.verbosity = 3
+        assert converter.settings.verbosity == 3
+                                                
+        converter.settings.WB_box = [1, 2, 3, 4]
+        assert converter.settings.WB_box == [1, 2, 3, 4]
+                                                        
+        converter.settings.crop_box = [5, 6, 7, 8]
+        assert converter.settings.crop_box == [5, 6, 7, 8]
+                                                        
+        custom_WB = [2.0, 1.0, 1.5, 1.0]
+        converter.settings.custom_WB = custom_WB
+        for i in range(4):
+            assert abs(converter.settings.custom_WB[i] - custom_WB[i]) < 0.001
+        
+        custom_matrix = [
+            [1.0, 0.1, 0.2],
+            [0.3, 1.1, 0.4],
+            [0.5, 0.6, 1.2]
+        ]
+        converter.settings.custom_matrix = custom_matrix
+        for i in range(3):
+            for j in range(3):
+                assert abs(converter.settings.custom_matrix[i][j] - custom_matrix[i][j]) < 0.001
+        
+        converter.settings.chromatic_aberration = [1.2, 2.3]
+        assert abs(converter.settings.chromatic_aberration[0] - 1.2) < 0.001
+        assert abs(converter.settings.chromatic_aberration[1] - 2.3) < 0.001
+        
 
 if __name__ == "__main__":
     pytest.main([__file__])

797fe30e19cc72329b894effe9ec1b1eefee5cdb
Aleksandr Motsjonov <soswow@gmail.com>
2025-12-13 13:27:57 +1100

    Fix windows install caching in CI
    Signed-off-by: Aleksandr Motsjonov <soswow@gmail.com>

--- build_scripts/install_deps_windows.bash
+++ build_scripts/install_deps_windows.bash
@@ -2,9 +2,16 @@
 
 set -ex
 
+# Configure vcpkg to use binary cache directory
+# This allows vcpkg to automatically reuse pre-built packages from cache
+export VCPKG_BINARY_SOURCES="clear;files,C:/vcpkg/binary-cache,readwrite"
+
+# Add/update baseline in vcpkg.json
 vcpkg x-update-baseline \
     --add-initial-baseline \
     --x-manifest-root="./build_scripts"
+
+# Install dependencies - vcpkg will automatically use binary cache if available
 vcpkg install \
     --x-install-root="C:/vcpkg/installed" \
     --x-manifest-root="./build_scripts"

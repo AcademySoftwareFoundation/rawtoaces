68ee747d415705ff9fc7f3cc553fe2eaa7a9082b
Anton Dukhovnikov <antond@wetafx.co.nz>
2026-01-27 18:10:37 +1300

    update exiftool url
    Signed-off-by: Anton Dukhovnikov <antond@wetafx.co.nz>

--- build_scripts/install_deps_windows.bash
+++ build_scripts/install_deps_windows.bash
@@ -20,7 +20,7 @@ VCPKG_PYTHON="C:/vcpkg/installed/x64-windows/tools/python3/python.exe"
 "$VCPKG_PYTHON" -m ensurepip --upgrade
 "$VCPKG_PYTHON" -m pip install pytest
 
-curl --silent --output ./exiftool.zip https://exiftool.org/exiftool-13.45_64.zip
+curl --silent --output ./exiftool.zip https://oliverbetz.de/cms/files/Artikel/ExifTool-for-Windows/exiftool-13.45_64.zip
 unzip ./exiftool.zip
 mv ./exiftool-13.45_64 ./exiftool
 mv ./exiftool/exiftool\(-k\).exe ./exiftool/exiftool.exe

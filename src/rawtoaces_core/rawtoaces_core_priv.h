30102cdd02f5f888b75b87c99b613435ad706144
Anton Dukhovnikov <antond@wetafx.co.nz>
2026-05-05 14:40:39 +1200

    [refactor] DNG solver
    Signed-off-by: Anton Dukhovnikov <antond@wetafx.co.nz>

--- src/rawtoaces_core/rawtoaces_core_priv.h
+++ src/rawtoaces_core/rawtoaces_core_priv.h
@@ -60,7 +60,8 @@ std::vector<std::vector<double>> XYZ_to_camera_weighted_matrix(
 
 std::vector<double> color_temperature_to_XYZ( const double &cct );
 
-std::vector<double> matrix_RGB_to_XYZ( const double chromaticities[][2] );
+std::vector<std::vector<double>>
+matrix_RGB_to_XYZ( const double chromaticities[][2] );
 
 std::vector<std::vector<double>>
 stack_rows( const std::vector<double> &matrix, size_t columns );

486ec5eaa0e27813e181e5f6c812a146ea75ffca
Anton Dukhovnikov <antond@wetafx.co.nz>
2026-05-21 08:18:00 +1200

    fix formatting
    Signed-off-by: Anton Dukhovnikov <antond@wetafx.co.nz>

--- src/rawtoaces_core/mathOps.h
+++ src/rawtoaces_core/mathOps.h
@@ -25,9 +25,9 @@ bool inverse(
         return false;
 
     size_t cols = src_mat[0].size();
-    if ( cols == 0 )
+    if ( rows != cols )
         return false;
-if (rows != cols) return false;
+
     Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> m;
     m.resize( rows, cols );
     for ( Eigen::Index i = 0; i < m.rows(); i++ )
